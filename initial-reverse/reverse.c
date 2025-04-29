f7599bc2cfb46755a17783749462c1d06151f8d8
Warnakulasuriya Fernando <98363154+Wfdo97@users.noreply.github.com>
2025-04-28 22:28:13 -0400

    Update reverse.c
    - added the functionality to output to a file (not tested)
    - refactored code

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -4,57 +4,76 @@
 
 int main(int argc, char* argv[]) {
 
-    if(argc > 3) {
-        fprintf(stderr, "usage: reverse <input> <output>");
-        exit(1);
-    }
-
-    /*====== Read File ======*/
-    FILE* file = fopen(argv[1], "r");
-    if(file == NULL) {
-        fprintf(stderr, "error: cannot open file 'input.txt'");
-        exit(1);
-    }
-
-    int capacity = 16;
     int count = 0;
+    int capacity = 16;
     char **strings = malloc(capacity * sizeof(*strings));
     if(strings == NULL) {
         printf("malloc failed"); 
         exit(1);
     }
 
-    char *line = NULL;
-    size_t len = 0;
+    if(argc > 3) {
+        fprintf(stderr, "usage: reverse <input> <output>");
+        exit(1);
+    }
+    else if(argc == 1) {
 
-    while(getline(&line, &len, file) != -1) {
-        if(count == capacity) {
-            capacity *= 2;
-            char **temp = realloc(strings, capacity * sizeof(*strings));
-            if(temp == NULL) {
-                perror("realloc didn't work properly");
+    }
+    else if(argc > 1) {
+        /*====== Read File ======*/
+        FILE* file1 = fopen(argv[1], "r");
+        if(file1 == NULL) {
+            fprintf(stderr, "error: cannot open file 'input.txt'");
+            exit(1);
+        }
+
+        char *line = NULL;
+        size_t len = 0;
+
+        while(getline(&line, &len, file1) != -1) {
+            if(count == capacity) {
+                capacity *= 2;
+                char **temp = realloc(strings, capacity * sizeof(*strings));
+                if(temp == NULL) {
+                    perror("realloc didn't work properly");
+                    break;
+                }
+                strings = temp;
+            }
+
+            strings[count] = malloc(strlen(line) + 1);
+            if(strings[count] == NULL) {
+                perror("malloc didn't work properly");
                 break;
             }
-            strings = temp;
+            strcpy(strings[count], line);
+            count++;
         }
 
-        strings[count] = malloc(strlen(line) + 1);
-        if(strings[count] == NULL) {
-            perror("malloc didn't work properly");
-            break;
+        free(line);
+        fclose(file1);
+    }
+
+    
+    if(argc == 3) {
+        FILE* file2 = fopen(argv[2], "w");
+        if(file2 == NULL) {
+            fprintf(stderr, "error: cannot open file 'output.txt'");
+            exit(1);
         }
-        strcpy(strings[count], line);
-        count++;
+
+        for(int i = count - 1; i >= 0; i--) {
+            fprintf(file2, strings[i]);
+            free(strings[i]);
+        }
+    }
+    else {
+        for(int i = count - 1; i >= 0; i--) {
+            printf("%s", strings[i]);
+            free(strings[i]);
+        }
     }
 
-    free(line);
-    fclose(file);
-
-    /*====== Condition 1:  ======*/
-    for(int i = count - 1; i >= 0; i--) {
-        printf("%s", strings[i]);
-        free(strings[i]);
-    }
     free(strings);
     return 0;
 }
