292ddd1379e55dea5250354bd43b2ab178acfbdd
Warnakulasuriya Fernando <98363154+Wfdo97@users.noreply.github.com>
2025-05-03 23:18:29 -0400

    Completed wgrep
    - implemented wgrep

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
+
+    if(argc == 1) {
+        printf("%s", "wgrep: searchterm [file ...]\n");
+        exit(1);
+    }
+
+    char* line = NULL;
+    size_t len = 0;
+
+    if(argc == 2) {
+        while(getline(&line, &len, stdin) != -1) {
+            if(strstr(line, argv[1]) != NULL) {
+                printf("%s", line);
+            }
+        }
+    }
+    else {
+        for(int i = 2; i < argc; i++) {
+            // printf("%s\n", argv[i]);
+            FILE* fp = fopen(argv[i], "r");
+            if(fp == NULL) {
+                printf("wgrep: cannot open file\n");
+                exit(1);
+            }
     
+            while(getline(&line, &len, fp) != -1) {
+                if(strstr(line, argv[1]) != NULL) {
+                    printf("%s", line);
+                }
+            }
+        }
+    }
     
     return 0;
 }
