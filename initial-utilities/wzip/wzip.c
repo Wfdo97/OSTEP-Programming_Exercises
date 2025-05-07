f871c23d511945351d9369bb540f20697d65c50e
Warnakulasuriya Fernando <98363154+Wfdo97@users.noreply.github.com>
2025-05-06 22:47:22 -0400

    updated wzip.c
    - added more functionality
    - can pass 3 tests but still need more work

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -3,71 +3,98 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-    
-    
 
+    if(argc == 1) {
+        printf("%s", "wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+    
     char* line = NULL;
     size_t len = 0;
     ssize_t nread;
     
-    size_t arrcap = 16;
-    size_t count = 0;
-    char** string = malloc(arrcap * sizeof(char*));
-    if(string == NULL) {
-        printf("malloc didn't work properly");
-        exit(1);
-    }
+    char* comb = NULL;
+    size_t resCap = 0;
+    size_t resLen = 0;
 
-    for(int i = 1; i < argc; i++) {
-        FILE* fp = fopen(argv[i], "r");
-        if(fp == NULL) {
-            printf("file cannot be open\n");
-            exit(1);
-        }
-
-        while((nread = getline(&line, &len, fp1)) != -1) {
-            
-            if(count >= arrcap) {
-                arrcap *= 2;
-                char** temp = realloc(string, arrcap * sizeof(char*));
-                if(temp == NULL) {
-                    printf("malloc didn't work properly");
-                    exit(1);
-                }
-                string = temp;
-            }
-
-            char* copy = malloc(nread + 1);
-            if(copy == NULL) {
-                printf("malloc didn't work properly");
+    // if(argc > 2) {
+        for(int i = 1; i < argc; i++) {
+            FILE* fp = fopen(argv[i], "r");
+            if(fp == NULL) {
+                printf("file cannot be open\n");
                 exit(1);
             }
 
-            memcpy(copy, string, nread + 1);
-            string[count++] = copy;
-        }
-        fclose(fp);
-    }
+            while((nread = getline(&line, &len, fp)) != -1) {
 
+                if(nread > 0 && line[nread - 1] == '\n') line[--nread] = '\0';
+
+                if(resLen + nread + 1 > resCap) {
+                    resCap = (resLen + nread + 1) * 2;
+                    char* tmp = realloc(comb, resCap);
+
+                    if(tmp == NULL) {
+                        printf("malloc didn't work properly\n");
+                        free(comb);
+                        free(line);
+                        exit(1);
+                    }
+                    comb = tmp;
+                }
+                memcpy(comb + resLen, line, nread);
+                resLen += nread;
+                comb[resLen] = '\n';
+            }
+            fclose(fp);
+        }
+
+        char temp = comb[0];
+        uint32_t count = 1;
+        for(int i = 1; i < resLen; i++) {
+            
+            if(comb[i] == temp) {
+                count++;
+            }
+            else {
+                fwrite(&count, sizeof(count), 1, stdout);
+                fwrite(&temp, sizeof(temp), 1, stdout);
+                temp = comb[i];
+                count = 1;
+            }
+        }
+        fwrite(&count, sizeof(count), 1, stdout);
+        fwrite(&temp, sizeof(temp), 1, stdout);
+
+        free(comb);
+    // }
+    // else if(argc == 2) {
+        
+    //     FILE* fp = fopen(argv[1], "r");
+    //         if(fp == NULL) {
+    //             printf("file cannot be open\n");
+    //             exit(1);
+    //         }
+
+    //     while((nread = getline(&line, &len, fp)) != -1) {
+    //         char temp = line[0];
+    //         uint32_t count = 1;
+    //         for(int i = 1; i < len; i++) {
+                
+    //             if(line[i] == temp) {
+    //                 count++;
+    //             }
+    //             else {
+    //                 fwrite(&count, sizeof(count), 1, stdout);
+    //                 fwrite(&temp, sizeof(temp), 1, stdout);
+    //                 temp = line[i];
+    //                 count = 1;
+    //             }
+    //         }
+    //         fwrite(&count, sizeof(count), 1, stdout);
+    //         fwrite(&temp, sizeof(temp), 1, stdout);
+    //     }
+    // }
     
-    char temp = string[0];
-    uint32_t count = 1;
-    for(int i = 1; i < sizeof(string); i++) {
-
-        if(string[i] == temp) {
-            count++;
-        }
-        else {
-            fwrite(&count, sizeof(count), 1, stdout);
-            fwrite(&temp, sizeof(temp), 1, stdout);
-            temp = line[i];
-            count = 1;
-        }
-    }
-    fwrite(&count, sizeof(count), 1, stdout);
-    fwrite(&temp, sizeof(temp), 1, stdout);
-
-    free(string);
-    // fclose(fp1);
+    free(line);
     return 0;
 }
