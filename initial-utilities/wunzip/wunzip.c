3b4051c20c70d7e6f1b4298687a4989d5dd14500
Warnakulasuriya Fernando <98363154+Wfdo97@users.noreply.github.com>
2025-05-10 22:59:47 -0400

    Update wunzip.c

--- initial-utilities/wunzip/wunzip.c
+++ initial-utilities/wunzip/wunzip.c
@@ -3,5 +3,24 @@
 
 int main(int argc, char* argv[]) {
     
+    if(argc > 2) {
+        printf("%s", "wunzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    FILE* fp = fopen(argv[1], "r");
+    if(fp == NULL) {
+        printf("%s", "cannot open this file\n");
+        exit(1);
+    }
+
+    char* line = NULL;
+    ssize_t nread;
+    size_t len = 0;
+
+    while((nread = getline(&line, &len, fp)) != -1) {
+        printf("%s", line);
+    }
+
     return 0;
 }
